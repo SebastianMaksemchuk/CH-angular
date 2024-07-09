c723310531242180a8ff66c84da40ba9d08090b0
Sebastian Maksemchuk <sebastian@netio.com.ar>
2024-07-09 09:37:59 -0300

    initial moduling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LayoutModule } from './layout/layout.module';
+import { GlobalModule } from './global/global.module';
+import { AuthModule } from './auth/auth.module';
+import { DashModule } from './dash/dash.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GlobalModule,
+    LayoutModule,
+    AuthModule,
+    DashModule
   ],
   providers: [
     provideAnimationsAsync()
